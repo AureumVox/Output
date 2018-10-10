d37bbe5eceaaf26a47849c42c403813ef3da4c3d
musikera4 <41711416+musikera4@users.noreply.github.com>
2018-10-10 13:19:01 +0800

    Add files via upload

---
+++ CAPSTONE/resources/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $("#navbar").css({
+ 
+                "opacity": "0"
+            })
+        } else {
+            $("#navbar").css({
+ 
+                "opacity": "1",
+ 
+            })
+ 
+        }
+    })
+ })
